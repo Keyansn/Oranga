c64315b35fb27e517e54a30264b78acda3c54bbe
Keyansn <keyansn@gmail.com>
2018-06-29 18:09:53 +0100

    Small improvements like aging

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,32 +14,93 @@ public class PlayerController : MonoBehaviour {
     public Transform player;
     public Vector3Int test;
 
+    public float energy = 100;
+    public float appitite = 0.1f;
+    public float moveCost = 4;
+    public float age;
+    public float lifespan;
+
+    public int ageGroup;
+    /*
+     * 0 = Baby
+     * 1 = Child
+     * 2 = Teenager
+     * 3 = Young Adult
+     * 4 = Adult
+     * 5 = Elderly
+     */
+
+
     // Use this for initialization
     void Start () {
         player = this.transform;
+        age = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        age = age + Time.deltaTime;
+
+        if (age > lifespan){Death();}
+        if (energy <= 0) { Death(); }
+
+
+        switch (ageGroup)
+        {
+            //////////////////////////////    0 = Baby           //////////////////////////////
+            case 0:
+                break;
+
+            //////////////////////////////    1 = Child          //////////////////////////////
+            case 1:
+                break;
+
+            //////////////////////////////    2 = Teenager       //////////////////////////////
+            case 2:
+                break;
+
+            //////////////////////////////    3 = Young Adult    //////////////////////////////
+            case 3:
+                break;
+
+            //////////////////////////////    4 = Adult          //////////////////////////////
+            case 4:
+                break;
+
+            //////////////////////////////    5 = Elderly        //////////////////////////////
+            case 5:
+                break;
+        }
+
+
+        
+
+        moveSpeed = energy / 20;
+
         if (Input.GetAxisRaw("Horizontal") > 0.1f|| Input.GetAxisRaw("Horizontal") < -0.1f)
         {
             transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            energy = energy - moveCost*Time.deltaTime;
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
         {
             transform.Translate(new Vector2(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
+            energy = energy - moveCost * Time.deltaTime;
         }
 
-        print((int)player.position.x / 2 + "   "+ (int)player.position.y / 2);
-        
-        print(tilemap.GetTile(FloorToInt(player.position / 1.75f)));
         test = FloorToInt(player.position/1.75f);
-        //print(tilemap.GetTile(new Vector3Int(0,0,0)));
 
-        tilemap.SetColor(test, Color.cyan);
-        tilemap.SetTile(test, tb);
+        TilemapController Script = GameObject.FindGameObjectWithTag("GameController").GetComponent<TilemapController>();
+        energy = energy + Script.Eat(test,appitite);
 
+        if (energy > 100){energy = 100;}
+    }
+
+    void Death()
+    {
+        gameObject.SetActive(false);
     }
 
 
--- Assets/Scripts/TilemapController.cs
+++ Assets/Scripts/TilemapController.cs
@@ -5,32 +5,110 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
-
-    public TileData tileDataHolder;
+    public IDictionary<string, MapTileClass> tileDictionary = new Dictionary <string, MapTileClass>();
+    //public List<maptileclass> tileInfo = new List<maptileclass>();
+    //public TileData tileDataHolder;
     public Tilemap tilemap;
-    public TileBase tb;
+    public TileBase highfood;
+    public TileBase midfood;
+    public TileBase lowfood;
 
     // Use this for initialization
-    void Start () {
-        createthemap();
-    }	
-	// Update is called once per frame
-	void Update () {
-        print(tilemap.localBounds);
+    void Start() {
+        Createthemap();
+    }
+    // Update is called once per frame
+    void Update()
+    {
 
+        print(tileDictionary["1,1"]);
+        print(tileDictionary["2,1"].food);
+        //print(tilemap.localBounds);
+        
+        for (int i = -5; i < 5; i++)
+        {
+            for (int j = -5; j < 5; j++)
+            {
+                //FOOD CALCULATIONS 
+
+                float foodHolder;
+                foodHolder = tileDictionary[i + "," + j].food;
+
+                foodHolder = foodHolder + 0.1f * Time.deltaTime;
+                if (foodHolder > 5)
+                {
+                    foodHolder = 5;
+                }
+
+                if (foodHolder >= 4)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 1), highfood);
+                }
+
+                if ((2 < foodHolder)&&(foodHolder<4))
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 1), midfood);
+                }
+
+                if (2 > foodHolder)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 1), lowfood);
+                }
+
+
+
+                tileDictionary[i + "," + j].food = foodHolder;
+            }
+        }
     }
 
-    void createthemap()
+    public float Eat(Vector3Int loc, float _appitite)
+    {
+        tileDictionary[loc.x + "," + loc.y].food = tileDictionary[loc.x + "," + loc.y].food - _appitite*Time.deltaTime;
+        if(tileDictionary[loc.x + "," + loc.y].food < 0)
+        {
+            tileDictionary[loc.x + "," + loc.y].food = 0;
+        }
+
+        return _appitite * Time.deltaTime;
+    }
+
+
+    void Createthemap()
     {
         for (int i = -5; i < 5; i++)
         {
             for (int j = -5; j < 5; j++)
             {
-                tilemap.SetTile(new Vector3Int(i, j, 1), tb);
+                tilemap.SetTile(new Vector3Int(i, j, 1), highfood);
+
+                string temp = i + "," + j;
+                
+                tileDictionary.Add(temp,new MapTileClass(1,1));
+                //coords.Clear();
             }
+
             //tilemap.SetTile(new Vector3Int(1,1,1), tb);
         }
     }
 
     
+}
+
+[System.Serializable]
+public class MapTileClass
+{
+
+    //  CLASS VARIABLES
+    public float food;
+    public float temp;
+
+    //  CLASS CONSTRUCTORS
+    public MapTileClass(float _food, float _temp)
+    {
+        food = _food;
+        temp = _temp;
+    }
+
+    //  CLASS METHODS
 }
